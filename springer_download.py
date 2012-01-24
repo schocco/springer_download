816e98863fa2a7fe48ac14aa216cedcd7d0b1e04
Rocco Schulz <schocco.rulz@googlemail.com>
2012-01-24 22:25:09 +0100

    Replaced sanitizeFilename() with os independent function. Cleaned up.
    Use urllib2 methods instead of SpringerUrlOpener, removed unused
    functions.

--- springer_download.py
+++ springer_download.py
@@ -6,6 +6,7 @@
 from time import sleep
 from urllib2 import HTTPError
 import getopt
+import mimetypes
 import os
 import re
 import shutil
@@ -13,35 +14,17 @@ import subprocess
 import sys
 import tempfile
 import threading
-import urllib
 import urllib2
-import mimetypes
 
 
 #CONFIG
-#TODO: add global conf
 tempDir = tempfile.mkdtemp()
 cwd = os.getcwd()
-
-# Set some kind of User-Agent so we don't get blocked by SpringerLink
-class SpringerURLopener(urllib.FancyURLopener):
-    version = "Mozilla 5.0"
     
 class Book(object):
     '''
     Representation of a single e-book.
     '''
-    book_hash = None
-    url = None
-    title = None
-    subtitle = None
-    isbn = None
-    chapters = list()
-    edition = None
-    year = None
-    publisher = None
-    cover_link = None
-    cover_path = None
     
     def __init__(self, book_hash):
         '''
@@ -49,9 +32,15 @@ class Book(object):
         :param url: absolute or relative url to the book
         '''
         self.book_hash = book_hash
+        self.title = ""
+        self.subtitle = ""
+        self.chapters = []
+        self.cover_link = None
+        
         self.url = "http://springerlink.com/content/%s/contents" % book_hash
         self._fetch_book_info()
         self._load_chapters()
+        self.cover_path = os.path.join(tempDir, "frontcover.pdf")
         
     def _load_chapters(self, page=None):
         'Creates chapter objects and appends them to self.chapters.'
@@ -63,7 +52,7 @@ class Book(object):
             chapterLink = match.group(1)
             if chapterLink[:7] == "http://": # skip external links
                 continue
-            
+                        
             if re.search(r'front-matter.pdf', chapterLink):
                 if front_matter:
                     continue
@@ -97,21 +86,17 @@ class Book(object):
         
     def _get_page(self, link):
         ':returns: html source of the given link.'
-        try:
-            print "get page..."
-            loader = SpringerURLopener()
-            page = loader.open(link).read()
-        except IOError, e:
-            error("Bad link given (%s)" % e)
-
-        if re.search(r'403 Forbidden', page):
-            error("Could not access page: 403 Forbidden error.")
-        
-        return page
+        txheaders = {'User-agent': 'Mozilla/5.0', }
+        req = urllib2.Request(link, headers=txheaders)
+        f = urllib2.urlopen(req)
+        return f.read()
           
     def _fetch_book_info(self):
-        '''Parses the books Web page to retrieve its information'''
-
+        '''
+        Parses the books Web page to retrieve its information.
+        
+        Values being set are: `title`, `subtitle`, `cover_link`
+        '''
         page = self._get_page(self.url)
         # get title
         match = re.search(r'<h1[^<]+class="title">(.+?)(?:<br/>\s*<span class="subtitle">(.+?)</span>\s*)?</h1>', page, re.S)
@@ -130,32 +115,31 @@ class Book(object):
         match = re.search(r'<div class="coverImage" title="Cover Image" style="background-image: url\(/content/([^/]+)/cover-medium\.gif\)">', page)
         if match:
             self.cover_link = "http://springerlink.com/content/" + match.group(1) + "/cover-large.gif"
-            #TODO: move cover download into a thread
-            print "downloading front cover from %s" % self.cover_link
-            dst = os.path.join(tempDir, "frontcover")
-            download(self.cover_link, dst)
-            if os.system("convert %s %s.pdf" % (dst, dst)) == 0:
-                self.cover_path = os.path.join("%s.pdf" % dst)
         
     def download(self, merge):
         '''
         Downloads all chapters to disk.
-        Starts one thread for each chapter
+        Also downloads the cover image and converts it to pdf.        
+        Uses one ```Downloader`` thread for each file.
         '''
         if os.path.isfile(self.path):
             error("%s already downloaded" % self.path)
 
         print "\nNow Trying to download book '%s'\n" % self.title
-       
+        #get cover
+        if self.cover_link:
+            d = Downloader(self.cover_link, self.cover_path,
+                           mimes = ("image/png", "image/gif", "image/jpg"),
+                           daemon = False)
+            d.start()
+        #get chapters
         for c in self.chapters:
-            print "downloading chapter %d/%d" % (c.index, len(self.chapters))
-            c.setDaemon(True)
-            c.start()
+            d = Downloader(c.url, c.path)
+            d.start()
         
         #wait for threads to finish
         while threading.activeCount() > 1:
             sleep(1)
-            print "sleeping"
             
         if merge:
             print "merging chapters"
@@ -180,7 +164,7 @@ class Book(object):
         '''
         :return: Path were the final pdf should be saved.
         '''
-        return "%s/%s.pdf" % (cwd, sanitizeFilename(self.title))
+        return "%s/%s.pdf" % (cwd, normalize(self.title))
     path = property(fget = get_path, doc = "Path were the final pdf should be saved.")
     
     def get_file_list(self):
@@ -189,8 +173,10 @@ class Book(object):
           Chapters with an empty path attribute are left out.
         '''
         fileList = []
-        if self.cover_path:
-            fileList.append(self.cover_path)
+        if self.cover_link:
+            dst = os.path.join(tempDir, "frontcover")
+            if os.system("convert %s. %s.pdf" % (dst, dst)) == 0:
+                fileList.append(self.cover_path)
         for c in self.chapters:
             if c.path:
                 fileList.append(c.path)
@@ -211,7 +197,6 @@ class Chapter(threading.Thread):
         self.url = url
         self.index = index
         self.path = os.path.join(tempDir, "%d.pdf" % self.index)
-        print "initialized chapter with index %d and url %s" % (self.index, self.url)
         
     def run(self):
         'Downloads the chapter as pdf'
@@ -340,6 +325,63 @@ def _reporthook(numblocks, blocksize, filesize, url=None):
         sys.stdout.write("\b"*70)
     sys.stdout.write("%-66s%3d%%" % (url, percent))
 
+
+class Downloader(threading.Thread):
+    '''
+    Downloader thread.
+    '''
+    def __init__(self, url, dst, mimes = [], daemon = True):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.dst = dst
+        self.setDaemon(daemon)
+        if not mimes:
+            self.mimes = mimes.append(mimetypes.guess_type(dst)[0])
+        else:
+            self.mimes = mimes
+        
+    def run(self):
+        '''
+        Starts the download from the specified url.
+        '''
+        txheaders = {   
+                 'User-agent': 'Mozilla/5.0',
+        }
+        req = urllib2.Request(self.url, headers=txheaders)
+        f = open(self.dst, 'wb')
+        try:
+            u = urllib2.urlopen(req)        
+            meta = u.info()
+            file_size = int(meta.getheaders("Content-Length")[0])
+            if self.mimes and meta.gettype() not in self.mimes:
+                error("Expected mimetype is %s, got %s instead!" % (self.mimes, meta.gettype()))
+            print "Downloading: %s : (%s Bytes)" % (self.url, file_size)
+        
+            file_size_dl = 0
+            block_sz = 8192
+            while True:
+                dl_buffer = u.read(block_sz)
+                if not dl_buffer:
+                    break
+        
+                file_size_dl += len(dl_buffer)
+                f.write(dl_buffer)
+                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                status = status + chr(8)*(len(status)+1)
+                print status,
+        except HTTPError, e:
+            print e
+            try:
+                shutil.rmtree(tempDir)
+            except IOError:
+                #already deleted
+                pass
+            error("%s - occured while downloading %s" % (e, self.url))
+        finally:
+            f.close()     
+    
+    
+    
 def download(url, dst, mime=None):
     '''
     Uses urllib2 to get a remote file.
@@ -349,11 +391,6 @@ def download(url, dst, mime=None):
         mime = mimetypes.guess_type(dst)[0]
     txheaders = {   
                  'User-agent': 'Mozilla/5.0',
-                 'Accept-Language': 'en-us',
-                 'Accept-Encoding': 'gzip, deflate, compress;q=0.9',
-                 'Keep-Alive': '300',
-                 'Connection': 'keep-alive',
-                 'Cache-Control': 'max-age=0',
     }
     req = urllib2.Request(url, headers=txheaders)
     f = open(dst, 'wb')
@@ -363,7 +400,7 @@ def download(url, dst, mime=None):
         file_size = int(meta.getheaders("Content-Length")[0])
         if mime and meta.gettype() != mime:
             error("Expected mimetype is %s, got %s instead!" % (mime, meta.gettype()))
-        print "Downloading: %s Bytes: %s" % (dst, file_size)
+        print "Downloading: %s : (%s Bytes)" % (url, file_size)
     
         file_size_dl = 0
         block_sz = 8192
@@ -389,23 +426,14 @@ def download(url, dst, mime=None):
         f.close()
 
 
+def normalize(value):
+    """
+    Coverts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+    """
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
 
-def geturl(url, dst):
-    ":DEPRECATED:"
-    downloader = SpringerURLopener()
-    if sys.stdout.isatty():
-        response = downloader.retrieve(url, dst,
-                           lambda nb, bs, fs, url=url: _reporthook(nb,bs,fs,url))
-        sys.stdout.write("\n")
-    else:
-        response = downloader.retrieve(url, dst)
-
-    return response
-
-def sanitizeFilename(filename):
-    p1 = subprocess.Popen(["echo", filename], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["iconv", "-f", "UTF-8", "-t" ,"ASCII//TRANSLIT"], stdin=p1.stdout, stdout=subprocess.PIPE)
-    return re.sub("\s+", "_", p2.communicate()[0].strip().replace("/", "-"))
 
 # start program
 if __name__ == "__main__":
